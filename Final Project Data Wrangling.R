5a4d3c98f632a569b06a192480a768e466fd6be8
johnk910 <146724801+johnk910@users.noreply.github.com>
2023-11-27 23:09:01 -0800

    Add files via upload

---
+++ Final Project Data Wrangling.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(stringr)
+
+wage_df <- read.csv("Minimum Wage Data (1).csv")
+inflation_df <- read.csv("seriesreport.csv")
+
+#Remove unnecessary column
+wage_df$Footnote = NULL
+
+# Remove rows where year is less than 2000
+wage_df <- subset(wage_df, Year >= 2000)
+
+#Get the average inflation (continous variable)
+inflation_df$Average_Inflation <- rowMeans(inflation_df[c("HALF1", "HALF2")], na.rm = TRUE)
+
+#Merge the data sets so that the average inflation from the inflation_df
+#is applied to its specific year in wage_df and is made into a new data set df
+df <- merge(wage_df, inflation_df[, c("Year", "Average_Inflation")], by = "Year", all.x = TRUE)
+
+#Categorical variable made
+#If the average percent is less than 2.0 the Inflation category column displays low inflation,
+#above 2.0 displays high inflation.
+df$Inflation_Category <- ifelse(df$Average_Inflation < 2.0, "Low Inflation", "High Inflation")
+
+#This dataframe summarizes each years inflation rate by its Federal Minimum Wage and CPI Average.
+
+sum_df <- aggregate(cbind(Average_Inflation, CPI.Average, Federal.Minimum.Wage) ~ Year, data = df, FUN = mean, na.rm = TRUE)
+
+ 
