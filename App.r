d874b967ffbd4a47cf8d30a5e36ae7c4c0df87b1
mattyi04 <150010172+mattyi04@users.noreply.github.com>
2023-12-09 20:39:20 -0800

    Create App.r

---
+++ App.r
@@ -0,0 +1,26 @@
+# Matthew Yi, Aiden Oh, John Kim 
+# 12-6-2023
+# I worked on this project with my teammates 
+
+library(dplyr)
+library(stringr)
+library(stringr)
+library(shiny)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+shinyApp(ui = ui, server = server) 
