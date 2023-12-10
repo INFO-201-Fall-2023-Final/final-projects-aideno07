de03b011b869b0d61639f0de44f69799729306b5
mattyi04 <150010172+mattyi04@users.noreply.github.com>
2023-12-09 23:10:36 -0800

    Update App.r

--- App.r
+++ App.r
@@ -10,17 +10,6 @@ library(ggplot2)
 
 ui <- fluidPage(
   titlePanel("Examining Minimum Wage Rates and Inflation Rates in the U.S.")
-  tabsetPanel(
-    tabPanel("Introduction"),
-    h2("About the Project"),
-    p("Over the past two decades, the alarming rise in US inflation has outpaced adjustments in job wages, leaving many citizens grappling with financial hardship. 
-      Soaring costs of living, coupled with stagnant wages, make it increasingly challenging to maintain financial stability, pushing individuals into a cycle of economic despair. 
-      The widening wage gap exacerbates poverty rates, a persistent issue in the US. As savings are depleted, unemployment rises, and social unrest grows, poverty and homelessness surge. 
-      The struggle for daily needs intensifies, impacting mental well-being, while businesses suffer from adjusting prices amid financial hardships. Despite acknowledging the issue, 
-      the US faces difficulties in finding effective solutions, raising the question: How much must the minimum wage increase to keep pace with inflation?"),
-    br(),
-    p("Through this project, we hope to highlight "),
-
     mainPanel(
     tabsetPanel(type = "tab",
                 tabPanel("Data", tableOutput("example")),
