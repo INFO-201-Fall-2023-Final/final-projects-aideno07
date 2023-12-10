0e0d58dbd90f24f74b481d234a2f347d3d707f8a
johnk910 <146724801+johnk910@users.noreply.github.com>
2023-12-10 02:10:38 -0800

    Update App.r
    Added Tab scatterplot.

--- App.r
+++ App.r
@@ -3,8 +3,6 @@
 # I worked on this project with my teammates 
 
 library(dplyr)
-library(stringr)
-library(plotly)
 library(shiny)
 library(ggplot2)
 
@@ -15,35 +13,40 @@ ui <- fluidPage(
                 tabPanel("Introduction", tableOutput("example"),
                          h2("About the project"),
                          p("Over the past two decades, the alarming rise in US inflation has outpaced adjustments in job wages, leaving many citizens grappling with financial hardship. 
-                          Soaring costs of living, coupled with stagnant wages, make it increasingly challenging to maintain financial stability, pushing individuals into a cycle of economic despair. 
-                          The growing difference between high and low wages makes poverty worse, which is a big problem in the US. As people's savings run out, more people lose their jobs, 
-                          and this causes more social problems. It leads to an increase in poverty and homelessness. The struggle for daily needs intensifies, impacting mental well-being, 
-                          while businesses suffer from adjusting prices amid financial hardships. Despite acknowledging the issue, the US faces difficulties in finding effective solutions, 
-                          raising the question: How much must the minimum wage increase to keep pace with inflation?"),
+                    Soaring costs of living, coupled with stagnant wages, make it increasingly challenging to maintain financial stability, pushing individuals into a cycle of economic despair. 
+                    The widening wage gap exacerbates poverty rates, a persistent issue in the US. As savings are depleted, unemployment rises, and social unrest grows, poverty and homelessness surge. 
+                    The struggle for daily needs intensifies, impacting mental well-being, while businesses suffer from adjusting prices amid financial hardships. Despite acknowledging the issue, 
+                    the US faces difficulties in finding effective solutions, raising the question: How much must the minimum wage increase to keep pace with inflation?"),
                          br(),
                          p("Through this project, we aim to explore the impact of inflation in different states within the United States. 
                       Thus, upon completion, we expect our project to reveal the relationship between minimum wage and inflation rates throughout the U.S. 
                       All in all, we believe that our study is of high importance because it illustrates how inflation and minimum wage rates are affecting low and middle-income communities. 
                       Due to the issue at hand, we desire to examine the problem more closely and identify any trends or patterns.")),
-                  tabPanel("Minimum Wage Rates", textOutput("Test"),  
-                          h2("the minimum wage has also increase in most places trying to keep up with the inflation rate")),
-                  tabPanel("Inflation Rates", textOutput("Test"),
-                          h2("At the same time, the inflation rate has also increased due to a multitude of reasons, Covid-19 in particular")), 
-                  tabPanel("Comparing the two datasets", textOutput("Test"),
-                          h2("Now lets look at the minimum wage and inflation rates side by side"))
+                
+                tabPanel("Minimum Wage Rates", 
+                         selectizeInput("selectedYears", "Select Years", choices = unique(sum_df$Year), multiple = TRUE),
+                         plotOutput("minWageScatterPlot")),
+                tabPanel("Inflation Rates", textOutput("inflationText")), 
+                tabPanel("Comparing the two datasets", textOutput("comparisonText"))
     )
   )
 )
 
 server <- function(input, output) {
-  
+  output$minWageScatterPlot <- renderPlot({
+    # Filter data based on selected years
+    selected_years_data <- filter(sum_df, Year %in% input$selectedYears)
+    
+    # Render a scatter plot of minimum wage rates for selected years
+    ggplot(selected_years_data, aes(x = as.factor(Year), y = Federal.Minimum.Wage)) +
+      geom_point(aes(color = factor(Year)), size = 3) +
+      scale_y_continuous(limits = c(0, max(sum_df$Federal.Minimum.Wage) * 1.2)) +
+      labs(title = "Minimum Wage Scatter Plot",
+           x = "Year",
+           y = "Federal Minimum Wage",
+           color = "Year in the US") +  # Set color legend title
+      theme(axis.text.x = element_text(angle = 0, hjust = 0.5))
+  })
 }
 
-
-
-
-
-
-
-
-shinyApp(ui = ui, server = server) 
+shinyApp(ui = ui, server = server)
